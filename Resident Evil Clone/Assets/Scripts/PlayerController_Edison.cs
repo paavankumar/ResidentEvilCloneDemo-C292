c127106249c8b16099dee70c894d8fa30dcf9a19
Excailibur <liedison360@gmail.com>
2024-09-19 10:16:12 -0400

    Made 3rd person controller

--- Resident Evil Clone/Assets/Scripts/PlayerController_Edison.cs
+++ Resident Evil Clone/Assets/Scripts/PlayerController_Edison.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class PlayerController_Edison : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float mouseSensity = 60f;
+
+    [SerializeField] float verticalLookLimit;
+    [SerializeField] Transform fpsCamera;
+
+    private bool isGrounded;
+    private float xRotation;
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+        LookAround();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    private void LookAround()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -verticalLookLimit, verticalLookLimit);
+
+        fpsCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    private void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        Vector3 moveVelocity = move * moveSpeed;
+
+        moveVelocity.y = rb.velocity.y;
+
+        rb.velocity = moveVelocity;
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
